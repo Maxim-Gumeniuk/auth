d58e7df6591d1a5d6a2470399f229f08bcba8fc6
Maxim Gumeniuk <108987194+Maxim-Gumeniuk@users.noreply.github.com>
2023-11-16 21:16:28 +0200

    login

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,8 @@ import cors from "cors";
 import { ENVVARIABLES } from "@/env-variables";
 import { authRouter } from "./routes/auth/auth-router";
 import { connectUsersDb } from "./db/users";
-<<<<<<< HEAD
 import { mainRouter } from "./routes/main";
 import { errorMiddleware } from "./middlewares/error";
-=======
-import { mainRouter } from "./routes/main/main-router";
->>>>>>> 33f38992519f5335762f31522a3d7733b62fe0ea
 
 const app = express();
 
@@ -23,10 +19,7 @@ app.use(cors({
 app.use(express.json());
 app.use(authRouter);
 app.use(mainRouter);
-<<<<<<< HEAD
 app.use(errorMiddleware)
-=======
->>>>>>> 33f38992519f5335762f31522a3d7733b62fe0ea
 
 const start = async (): Promise<void> => {
     try {
